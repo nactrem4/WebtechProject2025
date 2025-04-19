51ea956b1b60a706e616d59a983613f8c8e3419a
mertc <mert.can58@hotmail.de>
2025-04-19 22:47:08 +0200

    beginning

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-/* test 1*/
+/* test 2*/
 @SpringBootApplication
 public class DemoApplication {
 
